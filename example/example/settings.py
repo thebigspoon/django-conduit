61bdecb0c1b6673dd05e2dd5d9824f032a762fd8
Greg Corradini <gregcorradini@gmail.com>
2014-11-26 10:54:15 +0000

    make example application have default StandardTestRunner

--- example/example/settings.py
+++ example/example/settings.py
@@ -133,6 +133,8 @@ INSTALLED_APPS = (
     'example',
 )
 
+TEST_RUNNER = 'conduit.test.testrunners.StandardTestRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
